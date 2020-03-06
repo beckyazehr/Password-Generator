4854d77ae5251e4c1c8ea9b61732c4e32519f22a
Becky Zehr <beckyazehr@gmail.com>
2020-03-06 13:51:25 -0500

    Added prompts to newly writePassword function that was created.

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain? (8-128)")
+
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert("Your password will have " + passwordLength + " characters.")
+  }
+  else {
+    alert("Invalid selection. Please enter a numeric number that is at least 8 and no more than 128.")
+  }
+
+  var lowercase = confirm("Click OK to confirm including lowercase characters.");
+  var uppercase = confirm("Click OK to confirm including uppercase characters.");
+  var numeric = confirm("Click OK to confirm including numeric characters.");
+  var special = confirm("Click OK to confirm including special characters.");
+
+  if (lowercase === true || uppercase === true || number === true || special === true) {
+    alert("Create password now.")
+  }
+  else {
+    alert("You must select at least one character type.")
+  }
+}
